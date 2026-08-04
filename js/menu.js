3ee2ce2b4170f6a8c725577b2eb108b58b31f6a6
jules <mobliean236@gmail.com>
2026-08-05 02:46:31 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -4,136 +4,35 @@ console.log("Menu loaded");
 const products=[
 
 
-{
-name:"Espresso",
-price:40000,
-category:"coffee"
-},
-
-{
-name:"Americano",
-price:35000,
-category:"coffee"
-},
-
-{
-name:"Cappuccino",
-price:40000,
-category:"coffee"
-},
-
-{
-name:"Latte",
-price:40000,
-category:"coffee"
-},
-
-{
-name:"Mocha",
-price:45000,
-category:"coffee"
-},
-
-{
-name:"Vanilla Latte",
-price:45000,
-category:"coffee"
-},
-
-{
-name:"Caramel Latte",
-price:45000,
-category:"coffee"
-},
-
-{
-name:"Orange Coffee",
-price:45000,
-category:"coffee"
-},
-
-{
-name:"Coconut Coffee",
-price:45000,
-category:"coffee"
-},
+{name:"Espresso",price:40000,category:"coffee"},
+{name:"Americano",price:35000,category:"coffee"},
+{name:"Cappuccino",price:40000,category:"coffee"},
+{name:"Latte",price:40000,category:"coffee"},
+{name:"Mocha",price:45000,category:"coffee"},
+{name:"Vanilla Latte",price:45000,category:"coffee"},
+{name:"Caramel Latte",price:45000,category:"coffee"},
+{name:"Orange Coffee",price:45000,category:"coffee"},
+{name:"Coconut Coffee",price:45000,category:"coffee"},
 
 
-
-{
-name:"Matcha Coconut",
-price:50000,
-category:"matcha"
-},
-
-{
-name:"Matcha Latte",
-price:45000,
-category:"matcha"
-},
-
-{
-name:"Matcha Strawberry",
-price:50000,
-category:"matcha"
-},
-
-{
-name:"Green Tea",
-price:35000,
-category:"matcha"
-},
+{name:"Matcha Coconut",price:50000,category:"matcha"},
+{name:"Matcha Latte",price:45000,category:"matcha"},
+{name:"Matcha Strawberry",price:50000,category:"matcha"},
+{name:"Green Tea",price:35000,category:"matcha"},
 
 
-
-
-{
-name:"Cocoa Strawberry",
-price:45000,
-category:"noncoffee"
-},
-
-{
-name:"Cocoa Latte",
-price:35000,
-category:"noncoffee"
-},
-
-{
-name:"Cocoa Coconut",
-price:45000,
-category:"noncoffee"
-},
-
-{
-name:"Thai Milk Tea",
-price:35000,
-category:"noncoffee"
-},
-
-{
-name:"Lemon Thai Tea",
-price:35000,
-category:"noncoffee"
-},
-
-{
-name:"Strawberry Milk",
-price:35000,
-category:"noncoffee"
-}
-
-
+{name:"Cocoa Strawberry",price:45000,category:"noncoffee"},
+{name:"Cocoa Latte",price:35000,category:"noncoffee"},
+{name:"Cocoa Coconut",price:45000,category:"noncoffee"},
+{name:"Thai Milk Tea",price:35000,category:"noncoffee"},
+{name:"Lemon Thai Tea",price:35000,category:"noncoffee"},
+{name:"Strawberry Milk",price:35000,category:"noncoffee"}
 
 ];
 
 
 
-
-
-const menu=document.getElementById("coffee-menu");
-
-
+let menu=document.getElementById("coffee-menu");
 
 
 
@@ -143,32 +42,23 @@ function showCategory(category){
 menu.innerHTML="";
 
 
-
 products
-
-.filter(product=>product.category===category)
-
-.forEach(product=>{
+.filter(p=>p.category===category)
+.forEach(p=>{
 
 
-menu.innerHTML += `
-
+menu.innerHTML+=`
 
 <div class="card">
 
 
-<h3>
-${product.name}
-</h3>
+<h3>${p.name}</h3>
 
 
-<p>
-${product.price.toLocaleString()} Kip
-</p>
+<p>${p.price.toLocaleString()} Kip</p>
 
 
-
-<button onclick="openSugar('${product.name}',${product.price})">
+<button onclick="openSugar('${p.name}',${p.price})">
 
 ADD
 
@@ -177,16 +67,12 @@ ADD
 
 </div>
 
-
 `;
-
 
 });
 
 
 }
-
-
 
 
 
