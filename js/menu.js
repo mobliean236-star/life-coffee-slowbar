b6319b9f71aa966c7ced7ca721a9f433180bb360
jules <mobliean236@gmail.com>
2026-08-05 02:31:03 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,18 @@
 console.log("Menu loaded");
 
 
-const products=[
+const products = [
 
 
 {
 name:"Espresso",
 price:40000
+},
+
+
+{
+name:"Matcha Coconut",
+price:50000
 },
 
 
@@ -17,14 +23,74 @@ price:35000
 
 
 {
+name:"Matcha Latte",
+price:45000
+},
+
+
+{
 name:"Cappuccino",
 price:40000
 },
 
 
 {
+name:"Cocoa Strawberry",
+price:45000
+},
+
+
+{
 name:"Latte",
 price:40000
+},
+
+
+{
+name:"Cocoa Latte",
+price:35000
+},
+
+
+{
+name:"Mocha",
+price:45000
+},
+
+
+{
+name:"Matcha Strawberry",
+price:50000
+},
+
+
+{
+name:"Vanilla Latte",
+price:45000
+},
+
+
+{
+name:"Green Tea",
+price:35000
+},
+
+
+{
+name:"Caramel Latte",
+price:45000
+},
+
+
+{
+name:"Lemon Thai Tea",
+price:35000
+},
+
+
+{
+name:"Thai Milk Tea",
+price:35000
 },
 
 
@@ -37,6 +103,18 @@ price:45000
 {
 name:"Coconut Coffee",
 price:45000
+},
+
+
+{
+name:"Cocoa Coconut",
+price:45000
+},
+
+
+{
+name:"Strawberry Milk",
+price:35000
 }
 
 
@@ -57,12 +135,15 @@ menu.innerHTML += `
 <div class="card">
 
 
-<h3>${p.name}</h3>
+<h3>
+${p.name}
+</h3>
 
 
 <p>
 ${p.price.toLocaleString()} Kip
 </p>
+
 
 
 <button onclick="openSugar('${p.name}',${p.price})">
