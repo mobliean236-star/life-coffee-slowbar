36135a985efdab289627f61cee98022caa4f031d
jules <mobliean236@gmail.com>
2026-08-05 01:42:14 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,147 +6,35 @@ const coffee = [
 {
 name:"Espresso",
 price:40000,
-image:"images/espresso.jpg"
+image:"https://images.unsplash.com/photo-1510591509098-f4fdc6d0ff04?w=600"
 },
 
 {
 name:"Americano",
 price:35000,
-image:"images/americano.jpg"
+image:"https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=600"
 },
 
 {
 name:"Cappuccino",
 price:40000,
-image:"images/cappuccino.jpg"
+image:"https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?w=600"
 },
 
 {
 name:"Latte",
 price:40000,
-image:"images/latte.jpg"
-},
-
-{
-name:"Mocha",
-price:45000,
-image:"images/mocha.jpg"
-},
-
-{
-name:"Vanilla Latte",
-price:45000,
-image:"images/vanilla-latte.jpg"
-},
-
-{
-name:"Caramel Latte",
-price:45000,
-image:"images/caramel-latte.jpg"
+image:"https://images.unsplash.com/photo-1561882468-9110e03e0f78?w=600"
 },
 
 {
 name:"Orange Coffee",
 price:45000,
 best:true,
-image:"images/orange-coffee.jpg"
-},
-
-{
-name:"Coconut Coffee",
-price:45000,
-best:true,
-image:"images/coconut-coffee.jpg"
+image:"https://images.unsplash.com/photo-1517701604599-bb29b565090c?w=600"
 }
 
 ];
-
-
-
-
-
-const nonCoffee = [
-
-{
-name:"Matcha Coconut",
-price:50000,
-image:"images/matcha-coconut.jpg"
-},
-
-{
-name:"Matcha Latte",
-price:45000,
-image:"images/matcha-latte.jpg"
-},
-
-{
-name:"Matcha Strawberry",
-price:50000,
-image:"images/matcha-strawberry.jpg"
-},
-
-{
-name:"Cocoa Strawberry",
-price:45000,
-image:"images/cocoa-strawberry.jpg"
-},
-
-{
-name:"Cocoa Latte",
-price:35000,
-image:"images/cocoa.jpg"
-},
-
-{
-name:"Cocoa Coconut",
-price:45000,
-image:"images/cocoa-coconut.jpg"
-},
-
-{
-name:"Green Tea",
-price:35000,
-image:"images/green-tea.jpg"
-},
-
-{
-name:"Thai Milk Tea",
-price:35000,
-image:"images/thai-milk-tea.jpg"
-},
-
-{
-name:"Lemon Thai Tea",
-price:35000,
-image:"images/lemon-thai-tea.jpg"
-},
-
-{
-name:"Strawberry Milk",
-price:35000,
-image:"images/strawberry-milk.jpg"
-}
-
-];
-
-
-
-
-
-const extras = [
-
-{
-name:"+ Extra Shot",
-price:15000
-},
-
-{
-name:"+ Oat Milk",
-price:10000
-}
-
-];
-
 
 
 
@@ -154,21 +42,16 @@ price:10000
 const menu = document.getElementById("coffee-menu");
 
 
-const products = [
-...coffee,
-...nonCoffee
-];
 
-
-
-products.forEach(product => {
+coffee.forEach(product => {
 
 
 menu.innerHTML += `
 
 <div class="card">
 
-<img src="${product.image}" alt="${product.name}">
+
+<img src="${product.image}">
 
 
 <h3>
@@ -182,8 +65,11 @@ ${product.price.toLocaleString()} Kip
 </p>
 
 
+
 <button onclick="addToCart('${product.name}',${product.price})">
+
 ADD
+
 </button>
 
 
