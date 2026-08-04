89cec8279477fbf03db5cfc64258c4d18f9fa7ad
jules <mobliean236@gmail.com>
2026-08-05 02:24:14 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,54 @@
 console.log("Menu loaded");
 
 
-const coffee=[
+const products=[
+
 
 {
 name:"Espresso",
 price:40000
 },
 
+
 {
 name:"Americano",
 price:35000
 },
+
 
 {
 name:"Cappuccino",
 price:40000
 },
 
+
 {
 name:"Latte",
 price:40000
 },
+
 
 {
 name:"Orange Coffee",
 price:45000
 },
 
+
 {
 name:"Coconut Coffee",
 price:45000
 }
 
+
 ];
 
 
 
-const menu=document.getElementById("coffee-menu");
+let menu=document.getElementById("coffee-menu");
 
 
-coffee.forEach(product=>{
+
+products.forEach(p=>{
 
 
 menu.innerHTML += `
@@ -49,18 +57,15 @@ menu.innerHTML += `
 <div class="card">
 
 
-<h3>
-${product.name}
-</h3>
+<h3>${p.name}</h3>
 
 
 <p>
-${product.price.toLocaleString()} Kip
+${p.price.toLocaleString()} Kip
 </p>
 
 
-
-<button onclick="openSugarPopup('${product.name}',${product.price})">
+<button onclick="openSugar('${p.name}',${p.price})">
 
 ADD
 
