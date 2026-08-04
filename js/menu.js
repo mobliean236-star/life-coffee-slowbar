d42b96e16b40bab0c7829e0aa4ba737edc2cba81
jules <mobliean236@gmail.com>
2026-08-05 00:29:47 +0700

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,58 @@
+const coffee = [
+
+{
+name:"Espresso",
+price:35000,
+image:"https://images.unsplash.com/photo-1510591509098-f4fdc6d0ff04?w=600"
+},
+
+{
+name:"Americano",
+price:30000,
+image:"https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=600"
+},
+
+{
+name:"Cappuccino",
+price:35000,
+image:"https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?w=600"
+},
+
+{
+name:"Latte",
+price:35000,
+image:"https://images.unsplash.com/photo-1561882468-9110e03e0f78?w=600"
+},
+
+{
+name:"Orange Coffee",
+price:45000,
+best:true,
+image:"https://images.unsplash.com/photo-1517701604599-bb29b565090c?w=600"
+}
+
+];
+
+
+
+const nonCoffee = [
+
+{
+name:"Matcha Latte",
+price:45000,
+image:"https://images.unsplash.com/photo-1515823662972-da6a2e4d3002?w=600"
+},
+
+{
+name:"Thai Milk Tea",
+price:35000,
+image:"https://images.unsplash.com/photo-1544787219-7f47ccb76574?w=600"
+},
+
+{
+name:"Strawberry Milk",
+price:35000,
+image:"https://images.unsplash.com/photo-1558857563-b371033873b8?w=600"
+}
+
+];
